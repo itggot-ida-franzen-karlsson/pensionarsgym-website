e958963482951675768c3b747ef5f9a45c23b6f9
Ida Franzén Karlsson <ida.franzenkarlsson@itggot.se>
2018-03-16 14:02:43 +0100

    Added stasis and sass to my website

---
+++ site/public/js/main.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var menu = document.querySelector(".menu");
+    menu.classList.toggle("show");
+    var fade = document.querySelector(".fade");
+    fade.classList.toggle("visible");
+}
